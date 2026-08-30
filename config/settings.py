bdb776db0d3b27ebe6391b8b69eb9a9de0392ed6
Melissa <melissatakorabet101@gmail.com>
2026-08-31 01:06:07 +0200

    Allow Vercel hosts

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.1.6']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.1.6', '.vercel.app']
 
 
 # Application definition
